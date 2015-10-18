74ca462a1271eae4ba48c177b63f3cd85e14e96a
Travis Eubanks <tmeubanks113@gmail.com>
2015-10-18 12:35:51 -0500

    adds all models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ActiveRecord::Base
+   belongs_to :photo
+   belongs_to :commenter, :class_name => "User", :foreign_key => "comment_id"
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,6 @@
 class Photo < ActiveRecord::Base
+   belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
+
+   has_many :comments
+   has_many :favoritings, :dependent => :destroy
 end
